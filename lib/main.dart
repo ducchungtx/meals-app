19afbfa11c550cec0e7cca52560efdbba70aaade
Chung Nguyen <zrmedia9@gmail.com>
2021-11-04 14:36:02 +0700

    Project initial

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import './categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CategoriesScreen(),
+    );
+  }
+}
