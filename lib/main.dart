a7bbd1c80654e8ae2614ed185723ae792aa24dae
Chung Nguyen <zrmedia9@gmail.com>
2021-11-04 15:35:13 +0700

    Update font color setting

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: const TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 20,
+              ))),
       home: CategoriesScreen(),
     );
   }
